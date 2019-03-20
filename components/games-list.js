a686f7fb6b6b111d642c2a1c5b3ddc90e60be6fc
Pablo Vila <pablo.vilafer@gmail.com>
2019-03-20 09:24:33 +0100

    fixed centering of content

--- components/games-list.js
+++ components/games-list.js
@@ -2,7 +2,7 @@ import React from "react";
 import GameCard from "./game-card";
 
 export default ({ games }) => (
-  <div className="columns is-mobile is-multiline">
+  <div className="columns is-mobile is-multiline is-centered">
     {games.map(g => {
       return <GameCard key={g.id} game={g} />;
     })}
